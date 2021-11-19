a19c36aa5000939f7769a9969da6b76e1e9f141d
mazahacka2017 <mazahacka2017@gmail.com>
2021-11-19 22:09:58 +0400

    s

---
+++ simpocalypse/game_config/default_config.js
@@ -0,0 +1,5 @@
+// START original config for this file - do NOT change -> if needed for testing copy below instead
+ const GAME_PLATFORM = 'web'; // web, steam, ... itch?
+ const DEMO_VERSION = true; // true, false
+const PUBLISHER_PLATFORM = 'web'; // wev, steam.... itch
+// END original config
